7ed7d16371465a95e5237ac6c5a3effcb521cb8b
Charlie Jonas <charlie@charliejonas.co.uk>
2019-06-02 18:50:01 +0100

    Use log instead of panic

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"log"
 	"database/sql"
 	"github.com/joho/godotenv"
 	_ "github.com/go-sql-driver/mysql"
@@ -11,7 +12,7 @@ import (
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		panic(err.Error())
+		log.Fatal("Error loading .env file: ", err.Error())
 	}
 	mysqlUser := os.Getenv("MYSQL_USER")
 	mysqlPassword := os.Getenv("MYSQL_PASSWORD")
@@ -20,20 +21,20 @@ func main() {
 
 	db, err := sql.Open("mysql", connectionString)
 	if err != nil {
-		panic(err.Error())
+		log.Fatal("Error connecting to MySQL database: ", err.Error())
 	}
 	defer db.Close()
 
 	stmtOut, err := db.Prepare("SELECT COUNT(*) FROM queued_messages")
 	if err != nil {
-		panic(err.Error())
+		log.Fatal("Error preparing SQL statement: ", err.Error())
 	}
 	defer stmtOut.Close()
 
 	var queueLength int
 	err = stmtOut.QueryRow().Scan(&queueLength)
 	if err != nil {
-		panic(err.Error())
+		log.Fatal("Error performing query: ", err.Error())
 	}
 
 	fmt.Printf("The length of the Postal mail queue is: %d \n", queueLength)
